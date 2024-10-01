627da3ad3f6585d239518a7bc01f67a046ecaa7f
alecabnun <avestruz2265@gmail.com>
2024-10-01 12:06:03 +0200

    siuu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface MyLambda<A> {
+    A doMyLambda(String name);
+}
+
+class CreadorDeObjetos {
+    static <T> List<T> crear(MyLambda<T> lambda, List<String> names) {
+        List<T> objects = new ArrayList<>();
+
+        for(String name : names) {
+            objects.add(lambda.doMyLambda(name));
+        }
+
+        return objects;
+    }
+}
+
+record Ciudad (String name) {}
+
+class Album {
+    String titulo;
+    Album(String titulo){ this.titulo = titulo; }
+    public String toString() { return "Album{titulo='" + titulo + "'}"; }
+}
+
+class Coche {
+    String marca;
+    Coche(String marca){ this.marca = marca; }
+    public String toString() { return "Coche{marca='" + marca + "'}"; }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Crea les llistes
+
+        List<Ciudad> ciudades = CreadorDeObjetos.crear(Ciudad::new,
+                List.of("Paris", "Londres","Madird"));
+
+        var coches = CreadorDeObjetos.crear(Coche::new,
+                List.of("fiat", "renault", "nissan"));
+
+        // Imprimeix-les
+
+        ciudades.forEach(System.out::println);
+        coches.forEach(System.out::println);
+
     }
 }
